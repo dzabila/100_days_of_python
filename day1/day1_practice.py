a0f35acbe26d83400e343b5bdbb269137eb1e80b
dzabila <giorgi.dzabilashvili.1@btu.edu.ge>
2025-10-20 16:02:45 +0400

    day 1 practice

---
+++ day1/day1_practice.py
@@ -0,0 +1,30 @@
+# print statement
+
+print('Hello world!')
+
+
+# string manipulation
+print('Hello world! \nHello World')
+
+# string concatenation
+
+print('Hello' + 'Angela')
+
+print('Hello' + " " + 'angela')
+
+# python input Function
+print('Hello ' + input('What is your name? ') + "!")
+
+#variables
+name = "giorgi"
+
+# len function
+
+print(len('Dzabil;a'))
+
+#
+
+print(len(input("User input")))
+username = input('Enter your name ')
+length = len(username)
+print(length)
