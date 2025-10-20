01fc98b03859b5a6cd0c1be0ac46cf9783c5c895
dzabila <giorgi.dzabilashvili.1@btu.edu.ge>
2025-10-20 16:05:25 +0400

    day 1 project

---
+++ day1/day1_project.py
@@ -0,0 +1,9 @@
+# Day One Project
+# Band name generator
+
+print('Welcome to the band Name Generator.')
+
+city = input('What is the name of the city you grew up in?')
+pet_name = input("What is  your pet's name? ")
+
+print("Your band name could be " + city + ' ' + pet_name)
